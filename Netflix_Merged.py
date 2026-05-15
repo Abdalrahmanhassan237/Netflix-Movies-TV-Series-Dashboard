e00cea9b2c4dd46f21ecf1f26283553a8bef4208
AbdAlrahman Hassan <abdalrahman.hassan237@gmail.com>
2026-05-16 01:19:57 +0300

    fix the increase in the row shape after merge

--- Netflix_Merged.py
+++ Netflix_Merged.py
@@ -25,30 +25,40 @@ def clean_title(text):
     return text
 
 
-titles_df["title"] = titles_df["title"].apply(clean_title)
-ratings_df["title"] = ratings_df["title"].apply(clean_title)
-revenue_df["title"] = revenue_df["title"].apply(clean_title)
+titles_df["clean_title"] = titles_df["title"].apply(clean_title)
+ratings_df["clean_title"] = ratings_df["title"].apply(clean_title)
+revenue_df["clean_title"] = revenue_df["title"].apply(clean_title)
 
 movies_only_df = titles_df[titles_df["type"].str.lower().str.strip() == "movie"].copy()
 # 2. standarize the title key
 # for df in [titles_df, ratings_df, revenue_df]:
 # df["title"] = df["title"].str.lower().str.strip()
 
+# نحتفظ بأول نتيجة تظهر ونحذف التكرارات
+ratings_unique = ratings_df.drop_duplicates(subset=["clean_title"], keep="first")
+revenue_unique = revenue_df.drop_duplicates(subset=["clean_title"], keep="first")
+
 # 3. merge the files in one file
 merged_df = pd.merge(
     movies_only_df,
-    ratings_df[
-        ["title", "imdbAverageRating", "imdbNumVotes", "poster_path", "backdrop_path"]
+    ratings_unique[
+        [
+            "clean_title",
+            "imdbAverageRating",
+            "imdbNumVotes",
+            "poster_path",
+            "backdrop_path",
+        ]
     ],
-    on="title",
+    on="clean_title",
     how="left",
 )
 
 final_df = pd.merge(
     merged_df,
-    revenue_df[
+    revenue_unique[
         [
-            "title",
+            "clean_title",
             "rating",
             "votes",
             "budget",
@@ -57,8 +67,12 @@ final_df = pd.merge(
             "gross_us_canada",
         ]
     ],
-    on="title",
+    on="clean_title",
     how="left",
 )
 
 print(final_df.isnull().sum())
+print(f"original tiltles data : {titles_df.shape}")
+print(f"original movies_tiltles data : {movies_only_df.shape}")
+print(f"merged data : {merged_df.shape}")
+print(f"final data : {final_df.shape}")
